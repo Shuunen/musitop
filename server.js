d22bd4f477e7bdee155d9c6524698b5b3ea71a50
Romain Racamier Lafon <contact@romain.work>
2016-08-25 15:47:15 +0200

    handled read dir & shuffle

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+var shuffle = require('shuffle-array');
 var spawn = require('child_process').spawn;
 
+function playOneInFolder (folder) {
+    fs.readdir(folder, function (err, files) {
+        if (err) {
+            throw new Error(err);
+        }
+        var fileNames = [];
+        files.forEach(function (fileName) {
+            var filePath = path.join(folder, fileName);
+            var fileStat = fs.statSync(filePath);
+            if (fileStat.isFile()) {
+                fileNames.push(fileName)
+            }
+        });
+        playOneOfThese(fileNames);
+    });
+}
+
+function playOneOfThese (fileNames) {
+    if (doShuffle) {
+        shuffle(fileNames);
+    }
+    var fileName = fileNames[0];
+    var filePath = folder + '\\' + fileName;
+    playThisOne(filePath);
+}
+
+function playThisOne (filePath) {
+    var player = spawn('lib/1by1/1by1.exe', [filePath, '/hide', '/close']);
+    player.stderr.on('data', function (stderr) {
+        console.log("\n" + 'Stderr : ' + "\n" + stderr);
+    });
+    player.on('close', function (code) {
+        console.log('player process exited with code ' + code);
+    });
+}
+
+// init
 var folder = 'D:\\MiCloud\\Music\\brained to test';
-
-var music = 'Benny Smiles - Hotline Theme.mp3';
-
-var musicPath = folder + '\\' + music;
-
-// close  Close the program after playing one track
-// hide Hide the window and enable the Systray icon
-// 1by1.exe rain.mp3 /hide /close
-
-var player = spawn('lib/1by1/1by1.exe', [musicPath, '/hide', '/close']);
-
-player.stdout.on('data', function (stdout) {
-    console.log("\n" + 'Stdout : ' + "\n" + stdout);
-});
-
-player.stderr.on('data', function (stderr) {
-    console.log("\n" + 'Stderr : ' + "\n" + stderr);
-});
-
-player.on('close', function (code) {
-    console.log('player process exited with code ' + code);
-});
+var doShuffle = true;
+playOneInFolder(folder);
