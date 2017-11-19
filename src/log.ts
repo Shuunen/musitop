b22c34a0f091cc7920594c8801a5230285f1d799
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-11-19 17:00:57 +0100

    added log methods

--- src/log.ts
+++ src/log.ts
@@ -8,7 +8,10 @@ const pino = Pino({
 
 export class Log {
 
-    public static info(thing) {
-        pino.info(thing)
-    }
+    // From Pino doc : level (string): one of 'fatal', 'error', 'warn', 'info', 'debug', 'trace'
+
+    public static debug = pino.debug.bind(pino)
+    public static info = pino.info.bind(pino)
+    public static warn = pino.warn.bind(pino)
+    public static error = pino.error.bind(pino)
 }
