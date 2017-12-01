4f0490b1cef3657eb280374d60ab6dee6d3af053
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-12-01 17:50:54 +0100

    now app class has options

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-import { Server } from './server'
+'use strict'
 
-const instance = new Server()
+import Log from './log'
+import Server from './server'
+
+const defaultOptions = {
+    port: 1444,
+}
+
+export default class App {
+
+    protected options
+    protected server
+
+    constructor() {
+        Log.info('App : in constructor')
+        this.options = Object.assign({}, defaultOptions)
+        this.server = new Server(this.options)
+    }
+}
+
+const instance = new App()
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs'
 import * as  http2 from 'http2'
 import * as mime from 'mime-types'
 import * as path from 'path'
+import App from './app'
 import Log from './log'
 
 const {
@@ -13,50 +14,45 @@ const {
     HTTP_STATUS_INTERNAL_SERVER_ERROR,
 } = http2.constants
 
-const options = {
+const serverOptions = {
     cert: fs.readFileSync('./certs/server.crt'),
     key: fs.readFileSync('./certs/server.key'),
 }
 
 const serverRoot = './public'
 
-export class Server {
+export default class Server {
 
     private server
 
-    constructor() {
+    constructor(options) {
         Log.info('Server : in constructor')
-        this.create()
-        this.listen()
+        this.server = http2.createSecureServer(serverOptions)
+        this.server.on('stream', (stream, headers) => this.onStream(stream, headers))
+        this.server.listen(options.port)
+        Log.info('Server : listening on https://musitop.io:' + options.port)
+        return this.server
     }
 
-    private create() {
-        this.server = http2.createSecureServer(options)
-    }
-
-    private listen() {
-        this.server.on('stream', (stream, headers) => {
-            const reqPath = headers[HTTP2_HEADER_PATH]
-            const reqMethod = headers[HTTP2_HEADER_METHOD]
-            const fullPath = path.join(serverRoot, reqPath)
-            const responseMimeType = mime.lookup(fullPath)
-            Log.info('Server : path requested :', fullPath)
-            Log.info('Server : mime prepared :', responseMimeType)
-            if (!responseMimeType) {
-                Log.info('Server : serve text')
-                stream.respond({ 'content-type': 'text/plain' }, {
-                    onError: (err) => this.respondToStreamError(err, stream),
-                })
-                stream.end('hello you :)')
-            } else {
-                Log.info('Server : serve static file')
-                stream.respondWithFile(fullPath, { 'content-type': responseMimeType }, {
-                    onError: (err) => this.respondToStreamError(err, stream),
-                })
-            }
-        })
-        this.server.listen(443)
-        Log.info('Server : listening on https://musitop.io')
+    private onStream(stream, headers) {
+        const reqPath = headers[HTTP2_HEADER_PATH]
+        const reqMethod = headers[HTTP2_HEADER_METHOD]
+        const fullPath = path.join(serverRoot, reqPath)
+        const responseMimeType = mime.lookup(fullPath)
+        Log.info('Server : path requested :', fullPath)
+        Log.info('Server : mime prepared :', responseMimeType)
+        if (!responseMimeType) {
+            Log.info('Server : serve text')
+            stream.respond({ 'content-type': 'text/plain' }, {
+                onError: (err) => this.respondToStreamError(err, stream),
+            })
+            stream.end('hello you :)')
+        } else {
+            Log.info('Server : serve static file')
+            stream.respondWithFile(fullPath, { 'content-type': responseMimeType }, {
+                onError: (err) => this.respondToStreamError(err, stream),
+            })
+        }
     }
 
     private respondToStreamError(err, stream) {
