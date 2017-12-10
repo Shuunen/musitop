fd82b0e598bee1982d75b1f563aa6b827f342556
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-12-10 18:45:00 +0100

    added lot of tslint rules :p

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
 'use strict'
 
 import * as fs from 'fs'
-import * as  http2 from 'http2'
-import { Http2SecureServer, IncomingHttpHeaders } from 'http2'
+import { constants, createSecureServer, Http2SecureServer, IncomingHttpHeaders, ServerHttp2Stream } from 'http2'
 import * as mime from 'mime-types'
 import * as path from 'path'
 import App, { IAppOptions } from './app'
 import Log from './log'
 import Song from './song'
-
-const {
-    HTTP2_HEADER_PATH,
-    HTTP2_HEADER_METHOD,
-    HTTP_STATUS_NOT_FOUND,
-    HTTP_STATUS_INTERNAL_SERVER_ERROR,
-} = http2.constants
 
 const serverRoot: string = './public'
 
@@ -28,15 +20,15 @@ export default class Server {
             cert: fs.readFileSync(`./certs/${options.host}.crt`),
             key: fs.readFileSync(`./certs/${options.host}.key`),
         }
-        this.instance = http2.createSecureServer(serverOptions)
+        this.instance = createSecureServer(serverOptions)
         this.instance.on('stream', (stream, headers) => this.onStream(stream, headers))
         this.instance.listen(options.port)
         Log.info(`Server : listening on https://${options.host}:${options.port}`)
     }
 
-    onStream(stream: http2.ServerHttp2Stream, headers: IncomingHttpHeaders): void {
-        const reqPath: string = headers[HTTP2_HEADER_PATH].toString()
-        const reqMethod: string = headers[HTTP2_HEADER_METHOD].toString()
+    onStream(stream: ServerHttp2Stream, headers: IncomingHttpHeaders): void {
+        const reqPath: string = headers[constants.HTTP2_HEADER_PATH].toString()
+        const reqMethod: string = headers[constants.HTTP2_HEADER_METHOD].toString()
         const fullPath: fs.PathLike = path.join(serverRoot, reqPath)
         const responseMimeType: string = mime.lookup(fullPath)
         Log.info('Server : path requested :', fullPath)
@@ -53,13 +45,13 @@ export default class Server {
         }
     }
 
-    respondToStreamError(err: NodeJS.ErrnoException, stream: http2.ServerHttp2Stream): void {
+    respondToStreamError(err: NodeJS.ErrnoException, stream: ServerHttp2Stream): void {
         Log.info('Server : stream error')
         Log.info(err)
         if (err.code === 'ENOENT') {
-            stream.respond({ ':status': HTTP_STATUS_NOT_FOUND })
+            stream.respond({ ':status': constants.HTTP_STATUS_NOT_FOUND })
         } else {
-            stream.respond({ ':status': HTTP_STATUS_INTERNAL_SERVER_ERROR })
+            stream.respond({ ':status': constants.HTTP_STATUS_INTERNAL_SERVER_ERROR })
         }
         stream.end()
     }
